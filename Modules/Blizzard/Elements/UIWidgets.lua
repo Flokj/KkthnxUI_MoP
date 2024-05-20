8c29ed575465bc2fb64a025f28fe3e88ba35bdbc
Flokj <antohov1999@ukr.net>
2024-05-20 22:39:16 +0300

    Update UIWidgets.lua

--- Modules/Blizzard/Elements/UIWidgets.lua
+++ Modules/Blizzard/Elements/UIWidgets.lua
@@ -5,6 +5,24 @@ local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 local UIParent = UIParent
 
+local function topCenterPosition(self, _, b)
+	local holder = TopCenterContainerHolder
+	if b and (b ~= holder) then
+		self:ClearAllPoints()
+		self:SetPoint("CENTER", holder)
+		self:SetParent(holder)
+	end
+end
+
+local function belowMinimapPosition(self, _, b)
+	local holder = BelowMinimapContainerHolder
+	if b and (b ~= holder) then
+		self:ClearAllPoints()
+		self:SetPoint("CENTER", holder, "CENTER")
+		self:SetParent(holder)
+	end
+end
+
 -- Reanchor UIWidgets
 function Module:CreateUIWidgets()
 	-- Create a frame to move the UIWidgetFrame to a more desirable location
@@ -19,4 +37,27 @@ function Module:CreateUIWidgets()
 			self:SetPoint("TOPRIGHT", frame)
 		end
 	end)
+
+	local topCenterContainer = _G.UIWidgetTopCenterContainerFrame
+	local belowMiniMapcontainer = _G.UIWidgetBelowMinimapContainerFrame
+
+	local topCenterHolder = CreateFrame("Frame", "TopCenterContainerHolder", UIParent)
+	topCenterHolder:SetPoint("TOP", UIParent, "TOP", 0, -40)
+	topCenterHolder:SetSize(160, 30)
+
+	local belowMiniMapHolder = CreateFrame("Frame", "BelowMinimapContainerHolder", UIParent)
+	belowMiniMapHolder:SetPoint("TOP", UIParent, "TOP", 0, -80)
+	belowMiniMapHolder:SetSize(160, 30)
+
+	K.Mover(topCenterHolder, "TopCenterContainer", "TopCenterContainer", {"TOP", UIParent, "TOP", 0, -40}, 160, 30)
+	K.Mover(belowMiniMapHolder, "BelowMinimapContainer", "BelowMinimapContainer", {"TOP", UIParent, "TOP", 0, -80}, 160, 30)
+
+	topCenterContainer:ClearAllPoints()
+	topCenterContainer:SetPoint("CENTER", topCenterHolder)
+
+	belowMiniMapcontainer:ClearAllPoints()
+	belowMiniMapcontainer:SetPoint("CENTER", belowMiniMapHolder, "CENTER")
+
+	hooksecurefunc(topCenterContainer, "SetPoint", topCenterPosition)
+	hooksecurefunc(belowMiniMapcontainer, "SetPoint", belowMinimapPosition)
 end
