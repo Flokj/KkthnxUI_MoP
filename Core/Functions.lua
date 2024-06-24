cd978bc0aac2701ad75de159c173d8cb55618b88
Flokj <antohov1999@ukr.net>
2024-06-24 16:58:10 +0300

    add loot filter in chat

--- Core/Functions.lua
+++ Core/Functions.lua
@@ -326,10 +326,8 @@ end
 -- Item Level Functions
 do
 	local iLvlDB = {}
-	local enchantString = string_gsub(ENCHANTED_TOOLTIP_LINE, "%%s", "(.+)")
-	local essenceDescription = _G.GetSpellDescription(277253)
-	local essenceTextureID = 2975691
 	local itemLevelString = "^" .. string_gsub(ITEM_LEVEL, "%%d", "")
+	local RETRIEVING_ITEM_INFO = RETRIEVING_ITEM_INFO
 
 	function K.InspectItemTextures()
 		if not K.ScanTooltip.gems then
@@ -338,71 +336,41 @@ do
 			table_wipe(K.ScanTooltip.gems)
 		end
 
-		if not K.ScanTooltip.essences then
-			K.ScanTooltip.essences = {}
-		else
-			for _, essences in pairs(K.ScanTooltip.essences) do
-				table_wipe(essences)
-			end
-		end
-
-		local step = 1
-		for i = 1, 10 do
+		for i = 1, 5 do
 			local tex = _G[K.ScanTooltip:GetName() .. "Texture" .. i]
 			local texture = tex and tex:IsShown() and tex:GetTexture()
 			if texture then
-				if texture == essenceTextureID then
-					local selected = (K.ScanTooltip.gems[i - 1] ~= essenceTextureID and K.ScanTooltip.gems[i - 1]) or nil
-					if not K.ScanTooltip.essences[step] then
-						K.ScanTooltip.essences[step] = {}
-					end
-					K.ScanTooltip.essences[step][1] = selected -- essence texture if selected or nil
-					K.ScanTooltip.essences[step][2] = tex:GetAtlas() -- atlas place 'tooltip-heartofazerothessence-major' or 'tooltip-heartofazerothessence-minor'
-					K.ScanTooltip.essences[step][3] = texture -- border texture placed by the atlas
+				K.ScanTooltip.gems[i] = texture
+			end
+		end
 
-					step = step + 1
-					if selected then
-						K.ScanTooltip.gems[i - 1] = nil
+		return K.ScanTooltip.gems
+	end
+
+	function K:GetEnchantText(link, slotInfo)
+	local enchantID = tonumber(strmatch(link, "item:%d+:(%d+):"))
+	if enchantID then
+		--[[for i = 1, tip:NumLines() do
+			local line = _G["KKUI_ScanTooltipTextLeft"..i]
+			if not line then break end
+
+			local text = line:GetText()
+			if text then
+				if i == 1 and text == RETRIEVING_ITEM_INFO then
+					return "tooSoon"
+				elseif i ~= 1 then
+					local r, g, b = line:GetTextColor()
+					r = K:Round(r, 3)
+					g = K:Round(g, 3)
+					b = K:Round(b, 3)
+					if not (r == 1 and g == 1 and b == 1) then
+						return text
 					end
-				else
-					K.ScanTooltip.gems[i] = texture
 				end
 			end
+		end]]
+			return "+"
 		end
-
-		return K.ScanTooltip.gems, K.ScanTooltip.essences
-	end
-
-	function K.InspectItemInfo(text, slotInfo)
-		local itemLevel = string_find(text, itemLevelString) and string_match(text, "(%d+)%)?$")
-		if itemLevel then slotInfo.iLvl = tonumber(itemLevel) end
-
-		local enchant = string_match(text, enchantString)
-		if enchant then
-			slotInfo.enchantText = enchant
-		end
-	end
-
-	function K.CollectEssenceInfo(index, lineText, slotInfo)
-		local step = 1
-		local essence = slotInfo.essences[step]
-	-- stylua: ignore
-	if essence and next(essence) and (string_find(lineText, ITEM_SPELL_TRIGGER_ONEQUIP, nil, true) and string_find(lineText, essenceDescription, nil, true)) then
-		for i = 4, 2, -1 do
-			local line = _G[K.ScanTooltip:GetName() .. "TextLeft" .. index - i]
-			local text = line and line:GetText()
-
-			if text and (not string_match(text, "^[ +]")) and essence and next(essence) then
-				local r, g, b = line:GetTextColor()
-				essence[4] = r
-				essence[5] = g
-				essence[6] = b
-
-				step = step + 1
-				essence = slotInfo.essences[step]
-			end
-		end
-	end
 	end
 
 	function K.GetItemLevel(link, arg1, arg2, fullScan)
@@ -417,16 +385,8 @@ do
 			end
 
 			local slotInfo = K.ScanTooltip.slotInfo
-			slotInfo.gems, slotInfo.essences = K.InspectItemTextures()
-
-			for i = 1, K.ScanTooltip:NumLines() do
-				local line = _G[K.ScanTooltip:GetName() .. "TextLeft" .. i]
-				if line then
-					local text = line:GetText() or ""
-					K.InspectItemInfo(text, slotInfo)
-					K.CollectEssenceInfo(i, text, slotInfo)
-				end
-			end
+			slotInfo.gems = K.InspectItemTextures()
+			slotInfo.enchantText = K:GetEnchantText(link, slotInfo)
 
 			return slotInfo
 		else
@@ -441,6 +401,11 @@ do
 				K.ScanTooltip:SetBagItem(arg1, arg2)
 			else
 				K.ScanTooltip:SetHyperlink(link)
+			end
+
+			local firstLine = _G.KKUI_ScanTooltipTextLeft1:GetText()
+			if firstLine == RETRIEVING_ITEM_INFO then
+				return "tooSoon"
 			end
 
 			for i = 2, 5 do
