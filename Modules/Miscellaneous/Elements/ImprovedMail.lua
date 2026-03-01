9796370ff62aa4b31e044941f75d92eeb1062c56
Flokj <antohov1999@ukr.net>
2026-03-01 20:35:54 +0200

    Ambiguate realm name on contact list

--- Modules/Miscellaneous/Elements/ImprovedMail.lua
+++ Modules/Miscellaneous/Elements/ImprovedMail.lua
@@ -173,6 +173,7 @@ local contactListByRealm = {}
 
 function Module:ContactButton_OnClick()
 	local text = self.name:GetText() or ""
+	text = Ambiguate(text, "none")
 	SendMailNameEditBox:SetText(text)
 	SendMailNameEditBox:SetCursorPosition(0)
 end
